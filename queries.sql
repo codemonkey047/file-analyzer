0fb17b19d07be265a6117dbaaa9290886cad3fa8
codemonkey047@gmail.com <codemonkey047@gmail.com>
2021-01-18 19:15:39 -0500

    Add additional query, specify the external hard drive

--- queries.sql
+++ queries.sql
@@ -1,13 +1,12 @@
 select f1.filename, f1.path, f2.path
 from files as f1
 join files as f2 on f1.size = f2.size and f1.filename = f2.filename
-where f1.path > f2.path
-
-
+where f1.path < f2.path
+order by f1.extension, f1.path
 
 
 select f1.filename, f1.path, f2.path
 from files as f1
-join files as f2 on f1.size = f2.size and f1.filename = f2.filename
-where f1.path < f2.path
-order by f1.extension, f1.path
+left join files as f2 on f1.size = f2.size and f1.filename = f2.filename  and f2.source = 'desktop_s'
+where f1.source = 'external' and f2.source is NULL
+order by f1.path
--- src/runner.py
+++ src/runner.py
@@ -5,9 +5,9 @@ from sql_handler import SqlWrapper
 
 conn = sqlite3.connect('file.db')
 
-file_handler = SqlWrapper(conn, override_or_create=True, source='laptop')
+file_handler = SqlWrapper(conn, override_or_create=True, source='external')
 # file_handler = BaseFileScanHandler()
 
 scanner = FileScanner(file_handler, 15)
 
-scanner.process_dir("C:/Users/swill", 0)
+scanner.process_dir("G:\\", 0)
